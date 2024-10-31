be3987d295272a15bda82c8587c32ba2b6851f15
KevinDruart <k.druart2@gmail.com>
2024-10-31 15:41:29 +0100

    error update

--- src/modules/account/components/address-card/edit-address-modal.tsx
+++ src/modules/account/components/address-card/edit-address-modal.tsx
@@ -124,7 +124,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
 
       <Modal isOpen={state} close={close} data-testid="edit-address-modal">
         <Modal.Title>
-          <Heading className="mb-2">Editer l'adresse</Heading>
+          <Heading className="mb-2">Editer l&apos;adresse</Heading>
         </Modal.Title>
         <form action={formAction}>
           <Modal.Body>
